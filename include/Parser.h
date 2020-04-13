86864faa9adf6fdcbc77290e11ed411f6d6b9bfe
TzachSh <zach123@gmail.com>
2020-04-13 16:54:24 +0300

    Before implementation of stack operation counter.

--- include/Parser.h
+++ include/Parser.h
@@ -1,26 +1,25 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-
 #include "Constants.h"
 #include <spot/tl/parse.hh>
-#include <spot/tl/simplify.hh>
 #include <spot/tl/print.hh>
+#include <spot/tl/simplify.hh>
 #include <string>
 #include <vector>
 
+class Parser
+{
+  public:
+    Parser();
+    explicit Parser(std::vector<std::string>& formulas);
+    static spot::formula Simplify(const spot::formula& formula);
+    std::vector<spot::formula> Parse();
+    ~Parser();
 
-class Parser {
-public:
-  Parser();
-  explicit Parser(std::vector<std::string>& formulas);
-  std::vector<spot::formula> Parse();
-  ~Parser();
-
-private:
-  static spot::formula EliminateFG(spot::formula formula);
-  spot::formula Simplify(const spot::parsed_formula& parsedFormula);
-  std::vector<std::string> m_formulas;
+  private:
+    static spot::formula EliminateFG(spot::formula formula);
+    std::vector<std::string> m_formulas;
 };
 
 #endif
